52f8497a120ca3905679c6cef1a625c854726e34
artur <aartur.now@gmail.com>
2025-12-17 20:09:13 +0100

    feat: add models for user and market

--- backend/apps/market/admin.py
+++ backend/apps/market/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Company, Order, Transaction
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["active", "name", "full_name", "value"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['username', 'company_name', 'type', 'amount', 'price']
+
+    def username(self, obj):
+        return obj.user.nickname
+
+    def company_name(self, obj):
+        return obj.company.name
+
+admin.site.register(Transaction)
