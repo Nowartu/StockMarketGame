8941891be3c8c29650ea35cd152b43e789e412d6
artur <aartur.now@gmail.com>
2025-12-18 12:28:20 +0100

    feat: add user admin page

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import UserProfile, UserStock
 
-admin.site.register(UserProfile)
-admin.site.register(UserStock)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['nickname', 'user', 'balance', 'blocked_balance']
+
+    def user(self):
+        return self.user.username
+
+@admin.register(UserStock)
+class UserStockAdmin(admin.ModelAdmin):
+    list_display = ['user', 'company', 'amount', 'blocked']
+
+    def user(self):
+        return self.user.nickname
+
+    def company(self):
+        return self.company.name
