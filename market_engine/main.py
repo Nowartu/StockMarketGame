155b5347151f58921c5bfed1ac6969491718ce64
artur <aartur.now@gmail.com>
2026-01-06 17:56:41 +0100

    feat: integrate with redis pubsub

--- market_engine/main.py
+++ market_engine/main.py
@@ -1,9 +1,20 @@
-from database.models import *
 from database.session import get_db
-from sqlalchemy import select, and_, func, case
 from functions.orders import match_orders
 from functions.transactions import make_transaction
+import redis
 
-with get_db() as db:
-    order_1, order_2 = match_orders(db, 1)
-    make_transaction(db, order_1, order_2)
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    db=0,
+    password="smg"
+)
+p = r.pubsub()
+p.subscribe('new_order')
+print("Listening for messages")
+for message in p.listen():
+    print(f"message received ({message})")
+    with get_db() as db:
+        order_1, order_2 = match_orders(db, 1)
+        if order_1 is not None and order_2 is not None:
+            make_transaction(db, order_1, order_2)
