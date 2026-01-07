d26970238fdf423be1d4cca1847a3556c8824e92
artur <aartur.now@gmail.com>
2026-01-07 20:21:37 +0100

    feat: add stock url

--- backend/apps/market/urls.py
+++ backend/apps/market/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import OrderViewSet, CompanyList, TransactionList, ProfileList, UserStockList
+from .views import OrderViewSet, CompanyList, TransactionList, ProfileList, UserStockList, StockList
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -8,6 +8,7 @@ router.register(r'companies', CompanyList, basename="company")
 router.register(r'transactions', TransactionList, basename="transaction")
 router.register(f'profile', ProfileList, basename="profile")
 router.register(f'userstock', UserStockList, basename='userstock')
+router.register(f'stock', StockList, basename="stock")
 
 urlpatterns = [
     path("", include(router.urls))
