04521cb8373f21e10143118a812c91ae88c0c18c
artur <aartur.now@gmail.com>
2026-01-09 20:45:12 +0100

    feat: add event model to database

--- backend/apps/events/models.py
+++ backend/apps/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    type = models.CharField(max_length=100)
+    source = models.CharField(max_length=50)
+    reference_id = models.BigIntegerField(null=True, blank=True)
+    payload = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = '"event"."events"'
+
+    def __str__(self):
+        return f'{self.created_at}, {self.source}, {self.type}'
