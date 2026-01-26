e4f255bbf4fcbe5373f5be48722542644eb35563
artur <aartur.now@gmail.com>
2026-01-26 20:33:52 +0100

    feat: split to two consumerr, one for orders and another one for stock prices

--- backend/apps/market/consumers.py
+++ backend/apps/market/consumers.py
@@ -2,17 +2,57 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class AsyncChatConsumer(AsyncWebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.connected_users = []
-        self.users_config = {}
-
+class AsyncStockConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print(self.scope)
-        #self.connected_users.append(self.scope['user'])
+        #self.user = self.scope['user']
+        self.groups_subscribed = set()
+
         await self.accept()
 
     async def disconnect(self, code):
-        self.connected_users.remove(self.scope['user'])
+        for group in self.groups_subscribed:
+            await self.channel_layer.group_discard(group, self.channel_name)
+
+
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        action = data.get("action")
+        companies = data.get("companies", [])
+
+        if action == "subscribe":
+            for company_key in companies:
+                group_name = f"stock_{company_key.upper()}"
+                await self.channel_layer.group_add(group_name, self.channel_name)
+                self.groups_subscribed.add(group_name)
+                print(f"subscrbed to {group_name}")
+
+        elif action == "unsubscribe":
+            for company_key in companies:
+                group_name = f"stock_{company_key.upper()}"
+                await self.channel_layer.group_discard(group_name, self.channel_name)
+                self.groups_subscribed.discard(group_name)
+
+
+    async def stock_update(self, event):
+        print("Received")
+        await self.send(text_data=json.dumps(event))
+
+
+class AsyncOrderConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.group_name = f'user_{self.user}'
+
+        await self.channel_layer.group_add(
+            self.group_name, self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.group_name, self.channel_name
+        )
+
+    async def order_update(self, event):
+        pass
--- backend/apps/market/routing.py
+++ backend/apps/market/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/$", consumers.AsyncChatConsumer.as_asgi()),
+    re_path(r"ws/stock/$", consumers.AsyncStockConsumer.as_asgi()),
+    re_path(r"ws/order/$", consumers.AsyncOrderConsumer.as_asgi()),
 ]
